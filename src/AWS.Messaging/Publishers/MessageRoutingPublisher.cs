c2a9aeba0774367f7252a8203d08d06a60f170db
Philippe El Asmar <53088140+philasmar@users.noreply.github.com>
2023-02-16 13:05:05 -0500

    feat: add an implementation for an SQS publisher

---
+++ src/AWS.Messaging/Publishers/MessageRoutingPublisher.cs
@@ -0,0 +1,92 @@
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+using System.Collections.Concurrent;
+using AWS.Messaging.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace AWS.Messaging.Publishers;
+
+/// <summary>
+/// The message routing publisher allows publishing messages from application code to configured AWS services.
+/// It exposes the <see cref="PublishAsync{T}(T, CancellationToken)"/> method which takes in a user-defined message
+/// and looks up the corresponding <see cref="PublisherMapping"/> in order to route it to the appropriate AWS services.
+/// </summary>
+internal class MessageRoutingPublisher : IMessagePublisher
+{
+    private readonly IServiceProvider _serviceProvider;
+    private readonly IMessageConfiguration _messageConfiguration;
+    private readonly ILogger<IMessagePublisher> _logger;
+
+    /// <summary>
+    /// Creates an instance of <see cref="MessageRoutingPublisher"/>.
+    /// </summary>
+    public MessageRoutingPublisher(
+        IServiceProvider serviceProvider,
+        IMessageConfiguration messageConfiguration,
+        ILogger<IMessagePublisher> logger)
+    {
+        _serviceProvider = serviceProvider;
+        _messageConfiguration = messageConfiguration;
+        _logger = logger;
+    }
+
+    private readonly Dictionary<string, Type> _publisherTypeMapping = new()
+        {
+            { PublisherTargetType.SQS_PUBLISHER, typeof(SQSPublisher) }
+        };
+
+    /// <summary>
+    /// This dictionary serves as a method to cache created instances of <see cref="IMessagePublisher"/>,
+    /// to avoid having to create a new instance any time a message is published.
+    /// </summary>
+    private readonly ConcurrentDictionary<Type, IMessagePublisher> _publisherInstances = new();
+
+    /// <summary>
+    /// Publishes a user-defined message to an AWS service based on the
+    /// configuration done during startup. It retrieves the <see cref="PublisherMapping"/> corresponding to the
+    /// message type, which contains the routing information of the provided message.
+    /// The method wraps the message in a <see cref="MessageEnvelope"/> which contains metadata
+    /// that enables the proper transportation of the message throughout the framework.
+    /// </summary>
+    /// <param name="message">The message to be sent.</param>
+    /// <param name="token">The cancellation token used to cancel the request.</param>
+    public async Task PublishAsync<T>(T message, CancellationToken token = default)
+    {
+        var mapping = _messageConfiguration.GetPublisherMapping(typeof(T));
+        if (mapping == null)
+        {
+            _logger.LogError("The framework is not configured to publish messages of type '{0}'.", typeof(T).FullName);
+            throw new MissingMessageTypeConfigurationException($"The framework is not configured to publish messages of type '{typeof(T).FullName}'.");
+        }
+
+        if (_publisherTypeMapping.ContainsKey(mapping.PublishTargetType))
+        {
+            var publisherType = _publisherTypeMapping[mapping.PublishTargetType];
+            if (!typeof(IMessagePublisher).IsAssignableFrom(publisherType))
+            {
+                _logger.LogError("The message publisher corresponding to the type '{0}' is invalid " +
+                    "and does not implement the interface '{1}'.", mapping.PublishTargetType, typeof(IMessagePublisher));
+                throw new InvalidPublisherTypeException($"The message publisher corresponding to the type '{mapping.PublishTargetType}' is invalid " +
+                    $"and does not implement the interface '{typeof(IMessagePublisher)}'.");
+            }
+
+            var publisher = GetPublisherInstance(publisherType);
+            await publisher.PublishAsync(message, token);
+        }
+        else
+        {
+            _logger.LogError("The publisher type '{0}' is not supported.", mapping.PublishTargetType);
+            throw new UnsupportedPublisherException($"The publisher type '{mapping.PublishTargetType}' is not supported.");
+        }
+    }
+
+    private IMessagePublisher GetPublisherInstance(Type publisherType)
+    {
+        return _publisherInstances.GetOrAdd(publisherType, x =>
+        {
+            return (IMessagePublisher) ActivatorUtilities.CreateInstance(_serviceProvider, publisherType);
+        });
+    }
+}
---
+++ test/AWS.Messaging.UnitTests/MessagePublisherTests.cs
@@ -0,0 +1,108 @@
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+using System;
+using AWS.Messaging.Configuration;
+using Microsoft.Extensions.Logging;
+using Xunit;
+using Moq;
+using AWS.Messaging.Publishers;
+using System.Threading.Tasks;
+using AWS.Messaging.UnitTests.Models;
+using Amazon.SQS;
+using AWS.Messaging.Serialization;
+using System.Threading;
+using Amazon.SQS.Model;
+
+namespace AWS.Messaging.UnitTests;
+
+public class MessagePublisherTests
+{
+    private readonly Mock<IServiceProvider> _serviceProvider;
+    private readonly Mock<IMessageConfiguration> _messageConfiguration;
+    private readonly Mock<ILogger<IMessagePublisher>> _logger;
+    private readonly Mock<IAmazonSQS> _sqsClient;
+    private readonly Mock<IEnvelopeSerializer> _envelopeSerializer;
+    private readonly ChatMessage _chatMessage;
+
+    public MessagePublisherTests()
+    {
+        _serviceProvider = new Mock<IServiceProvider>();
+        _messageConfiguration = new Mock<IMessageConfiguration>();
+        _logger = new Mock<ILogger<IMessagePublisher>>();
+        _sqsClient = new Mock<IAmazonSQS>();
+        _envelopeSerializer = new Mock<IEnvelopeSerializer>();
+        _chatMessage = new ChatMessage { MessageDescription = "Test Description" };
+    }
+
+    [Fact]
+    public async Task SQSPublisher_HappyPath()
+    {
+        SetupSQSPublisherDIServices();
+
+        _sqsClient.Setup(x => x.SendMessageAsync(It.IsAny<SendMessageRequest>(), It.IsAny<CancellationToken>()));
+
+        var messagePublisher = new MessageRoutingPublisher(
+            _serviceProvider.Object,
+            _messageConfiguration.Object,
+            _logger.Object
+            );
+
+        await messagePublisher.PublishAsync(_chatMessage);
+
+        _sqsClient.Verify(x => x.SendMessageAsync(It.IsAny<SendMessageRequest>(), It.IsAny<CancellationToken>()), Times.Exactly(1));
+    }
+
+    [Fact]
+    public async Task SQSPublisher_MappingNotFound()
+    {
+        var messagePublisher = new MessageRoutingPublisher(
+            _serviceProvider.Object,
+            _messageConfiguration.Object,
+            _logger.Object
+            );
+
+        await Assert.ThrowsAsync<MissingMessageTypeConfigurationException>(() => messagePublisher.PublishAsync(_chatMessage));
+    }
+
+    [Fact]
+    public async Task SQSPublisher_InvalidMessage()
+    {
+        SetupSQSPublisherDIServices();
+
+        var messagePublisher = new MessageRoutingPublisher(
+            _serviceProvider.Object,
+            _messageConfiguration.Object,
+            _logger.Object
+            );
+
+        await Assert.ThrowsAsync<InvalidMessageException>(() => messagePublisher.PublishAsync<ChatMessage?>(null));
+    }
+
+    private void SetupSQSPublisherDIServices()
+    {
+        var publisherConfiguration = new SQSPublisherConfiguration("endpoint");
+        var publisherMapping = new PublisherMapping(typeof(ChatMessage), publisherConfiguration, PublisherTargetType.SQS_PUBLISHER);
+
+        _serviceProvider.Setup(x => x.GetService(typeof(IAmazonSQS))).Returns(_sqsClient.Object);
+        _serviceProvider.Setup(x => x.GetService(typeof(ILogger<IMessagePublisher>))).Returns(_logger.Object);
+        _serviceProvider.Setup(x => x.GetService(typeof(IMessageConfiguration))).Returns(_messageConfiguration.Object);
+        _serviceProvider.Setup(x => x.GetService(typeof(IEnvelopeSerializer))).Returns(_envelopeSerializer.Object);
+        _messageConfiguration.Setup(x => x.GetPublisherMapping(typeof(ChatMessage))).Returns(publisherMapping);
+    }
+
+    [Fact]
+    public async Task SQSPublisher_UnsupportedPublisher()
+    {
+        var publisherMapping = new PublisherMapping(typeof(ChatMessage), null!, "NEW_PUBLISHER");
+        _messageConfiguration.Setup(x => x.GetPublisherMapping(typeof(ChatMessage))).Returns(publisherMapping);
+
+        var messagePublisher = new MessageRoutingPublisher(
+            _serviceProvider.Object,
+            _messageConfiguration.Object,
+            _logger.Object
+            );
+
+        await Assert.ThrowsAsync<UnsupportedPublisherException>(() => messagePublisher.PublishAsync(_chatMessage));
+    }
+}
