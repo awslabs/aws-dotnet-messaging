a483042838e9c81c14405f0a8c45be4015663232
Alex Shovlin <shovlia@amazon.com>
2023-11-10 11:57:49 -0500

    Move to file-scoped namespaces in new classes

--- src/AWS.Messaging.Telemetry.OpenTelemetry/OpenTelemetryProvider.cs
+++ src/AWS.Messaging.Telemetry.OpenTelemetry/OpenTelemetryProvider.cs
@@ -5,80 +5,79 @@ using System.Diagnostics;
 using OpenTelemetry;
 using OpenTelemetry.Context.Propagation;
 
-namespace AWS.Messaging.Telemetry.OpenTelemetry
+namespace AWS.Messaging.Telemetry.OpenTelemetry;
+
+/// <summary>
+/// Creates OpenTelemetry traces
+/// </summary>
+public class OpenTelemetryProvider : ITelemetryProvider
 {
-    /// <summary>
-    /// Creates OpenTelemetry traces
-    /// </summary>
-    public class OpenTelemetryProvider : ITelemetryProvider
+    private static readonly ActivitySource _activitySource = new ActivitySource(Constants.SourceName, TelemetryKeys.AWSMessagingAssemblyVersion);
+
+    /// <inheritdoc/>
+    public ITelemetryTrace Trace(string traceName)
     {
-        private static readonly ActivitySource _activitySource = new ActivitySource(Constants.SourceName, TelemetryKeys.AWSMessagingAssemblyVersion);
-
-        /// <inheritdoc/>
-        public ITelemetryTrace Trace(string traceName)
+        var activity = _activitySource.StartActivity(traceName, ActivityKind.Producer);
+        if (activity != null)
         {
-            var activity = _activitySource.StartActivity(traceName, ActivityKind.Producer);
-            if (activity != null)
-            {
-                return new OpenTelemetryTrace(activity);
-            }
-
-            // If we initially failed to create an activity, attempt to force creation with 
-            // a link to the current activity, see https://opentelemetry.io/docs/instrumentation/net/manual/#creating-new-root-activities
-            var parentActivity = Activity.Current;
-            Activity.Current = null;
-            ActivityLink[]? links = null;
-            if (parentActivity != null)
-            {
-                links = new[] { new ActivityLink(parentActivity.Context) };
-            }
-
-            activity = _activitySource.StartActivity(traceName, ActivityKind.Producer, parentContext: default, links: links);
-            
-            return new OpenTelemetryTrace(activity, parentActivity);
+            return new OpenTelemetryTrace(activity);
         }
 
-        /// <inheritdoc/>
-        public ITelemetryTrace Trace(string traceName, MessageEnvelope envelope)
+        // If we initially failed to create an activity, attempt to force creation with 
+        // a link to the current activity, see https://opentelemetry.io/docs/instrumentation/net/manual/#creating-new-root-activities
+        var parentActivity = Activity.Current;
+        Activity.Current = null;
+        ActivityLink[]? links = null;
+        if (parentActivity != null)
         {
-            var propogatedContext = Propagators.DefaultTextMapPropagator.Extract(default, envelope, ExtractTraceContextFromEnvelope);
-            Baggage.Current = propogatedContext.Baggage;
-
-            var activity = _activitySource.StartActivity(traceName, ActivityKind.Consumer, parentContext: propogatedContext.ActivityContext);
-            if (activity != null)
-            {
-                return new OpenTelemetryTrace(activity);
-            }
-
-            // If we initially failed to create an activity, attempt to force creation with 
-            // a link to the current activity, see https://opentelemetry.io/docs/instrumentation/net/manual/#creating-new-root-activities
-            var parentActivity = Activity.Current;
-            Activity.Current = null;
-            ActivityLink[]? links = null;
-            if (parentActivity != null)
-            {
-                links = new[] { new ActivityLink(parentActivity.Context) };
-            }
-
-            activity = _activitySource.StartActivity(traceName, ActivityKind.Consumer, parentContext: propogatedContext.ActivityContext, links: links);
-
-            return new OpenTelemetryTrace(activity, parentActivity);
+            links = new[] { new ActivityLink(parentActivity.Context) };
         }
 
-        /// <summary>
-        /// Extracts propogation context from a <see cref="MessageEnvelope"/>, meant to be used with <see cref="TextMapPropagator"/>
-        /// </summary>
-        /// <param name="envelope">Inbound message envelope</param>
-        /// <param name="key">Context key</param>
-        /// <returns>Context value</returns>
-        private IEnumerable<string> ExtractTraceContextFromEnvelope(MessageEnvelope envelope, string key)
-        {
-            if (envelope.Metadata.TryGetValue(key, out var jsonElement))
-            {
-                return new string[] { jsonElement.ToString() };
-            }
+        activity = _activitySource.StartActivity(traceName, ActivityKind.Producer, parentContext: default, links: links);
+        
+        return new OpenTelemetryTrace(activity, parentActivity);
+    }
 
-            return Enumerable.Empty<string>();
+    /// <inheritdoc/>
+    public ITelemetryTrace Trace(string traceName, MessageEnvelope envelope)
+    {
+        var propogatedContext = Propagators.DefaultTextMapPropagator.Extract(default, envelope, ExtractTraceContextFromEnvelope);
+        Baggage.Current = propogatedContext.Baggage;
+
+        var activity = _activitySource.StartActivity(traceName, ActivityKind.Consumer, parentContext: propogatedContext.ActivityContext);
+        if (activity != null)
+        {
+            return new OpenTelemetryTrace(activity);
         }
+
+        // If we initially failed to create an activity, attempt to force creation with 
+        // a link to the current activity, see https://opentelemetry.io/docs/instrumentation/net/manual/#creating-new-root-activities
+        var parentActivity = Activity.Current;
+        Activity.Current = null;
+        ActivityLink[]? links = null;
+        if (parentActivity != null)
+        {
+            links = new[] { new ActivityLink(parentActivity.Context) };
+        }
+
+        activity = _activitySource.StartActivity(traceName, ActivityKind.Consumer, parentContext: propogatedContext.ActivityContext, links: links);
+
+        return new OpenTelemetryTrace(activity, parentActivity);
+    }
+
+    /// <summary>
+    /// Extracts propogation context from a <see cref="MessageEnvelope"/>, meant to be used with <see cref="TextMapPropagator"/>
+    /// </summary>
+    /// <param name="envelope">Inbound message envelope</param>
+    /// <param name="key">Context key</param>
+    /// <returns>Context value</returns>
+    private IEnumerable<string> ExtractTraceContextFromEnvelope(MessageEnvelope envelope, string key)
+    {
+        if (envelope.Metadata.TryGetValue(key, out var jsonElement))
+        {
+            return new string[] { jsonElement.ToString() };
+        }
+
+        return Enumerable.Empty<string>();
     }
 }
