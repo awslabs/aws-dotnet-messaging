6d0d2023bef86aae3fb59a04565553b22fef087f
brendonparker <brendon.parker@rfsmart.com>
2024-07-25 12:33:21 -0400

    Add autover

--- src/AWS.Messaging.Lambda/Services/DefaultLambdaMessageProcessor.cs
+++ src/AWS.Messaging.Lambda/Services/DefaultLambdaMessageProcessor.cs
@@ -218,7 +218,7 @@ internal class DefaultLambdaMessageProcessor : ILambdaMessageProcessor, ISQSMess
         if(!messages.Any())
         {
             return;
-        }    
+        }
 
         var request = new DeleteMessageBatchRequest
         {
