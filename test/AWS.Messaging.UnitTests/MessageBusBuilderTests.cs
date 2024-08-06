2a3b7ac47873f8ad2f3846079e0e8f79b77dda66
brendonparker <brendon.parker@rfsmart.com>
2024-08-06 09:24:52 -0400

    Undo

--- test/AWS.Messaging.UnitTests/MessageBusBuilderTests.cs
+++ test/AWS.Messaging.UnitTests/MessageBusBuilderTests.cs
@@ -4,7 +4,6 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Runtime.CompilerServices;
 using System.Text.Json;
 using Amazon.EventBridge;
 using Amazon.Extensions.NETCore.Setup;
