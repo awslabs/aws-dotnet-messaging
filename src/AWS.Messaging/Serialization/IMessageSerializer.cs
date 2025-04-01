72416ba5591e176795239bb649fd07c139c3940a
Garrett Beatty <gcbeatty@amazon.com>
2025-04-01 11:30:21 -0400

    update doc

--- src/AWS.Messaging/Serialization/IMessageSerializer.cs
+++ src/AWS.Messaging/Serialization/IMessageSerializer.cs
@@ -9,9 +9,10 @@ namespace AWS.Messaging.Serialization;
 public interface IMessageSerializer
 {
     /// <summary>
-    /// Serializes the .NET message object into a string.
+    /// Serializes the .NET message object into a string and specifies the content type of the serialized data.
     /// </summary>
     /// <param name="message">The .NET object that will be serialized.</param>
+    /// <returns>A <see cref="MessageSerializerResults"/> containing the serialized string and its content type.</returns>
     MessageSerializerResults Serialize(object message);
 
     /// <summary>
